c143dc4b02fc98481adc33aa7c991f11064d5afa
Jasmin-potager <jasmin@lepotager.org>
2026-09-04 21:30:41 +0200

    Clean companion widget preferences when removed

--- app/src/main/java/org/lepotager/executivefunction/widget/CompanionWidgetReceiver.kt
+++ app/src/main/java/org/lepotager/executivefunction/widget/CompanionWidgetReceiver.kt
@@ -1,8 +1,16 @@
 package org.lepotager.executivefunction.widget
 
+import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 
 class CompanionWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = CompanionWidget()
+
+    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
+        appWidgetIds.forEach { appWidgetId ->
+            CompanionWidgetPreferences.delete(context, appWidgetId)
+        }
+        super.onDeleted(context, appWidgetIds)
+    }
 }
